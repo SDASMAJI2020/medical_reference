46dc5f6ab6dbc7bd41087cc2732b35c30e300c30
sourav.iss2015 <sourav.iss2015@gmail.com>
2020-10-08 13:18:40 +0530

    first commit

---
+++ Controllers/PuchaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPMEDICAL.Controllers
+{
+    public class PuchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Vendor()
+        {
+            return View();
+        }
+
+        public IActionResult Item()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ReportController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPMEDICAL.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult DailyStock()
+        {
+            return View();
+        }
+        public IActionResult MonthlyStock()
+        {
+            return View();
+        }
+        public IActionResult Purcahse()
+        {
+            return View();
+        }
+        public IActionResult Sales()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Get()
+        {
+            var path = Path.Combine(
+            Directory.GetCurrentDirectory(), "wwwroot\\pdf\\game2.pdf");
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/pdf", "Demo.pdf");
+        }
+    }
+}
